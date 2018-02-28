6c8eb441ce225e72712967f982a6ac1adc7ff7f5
Igor <igor.linnyk@oracle.com>
2018-02-28 17:01:27 +0200

    async pipe preview panel added

--- src/app/preview-app/app-preview-panel.component.ts
+++ src/app/preview-app/app-preview-panel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { GetPicturesService } from '../get-pictures.service';
 import { PictureList } from '../picture-list-interface';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-preview-panel',
@@ -9,7 +10,7 @@ import { PictureList } from '../picture-list-interface';
 })
 
 export class AppPreviewPanelComponent implements OnInit {
-  pictureList: PictureList[];
+  pictureList: Observable<PictureList[]>;
   error: any;
 
   @Output() switchMode = new EventEmitter();
@@ -21,11 +22,13 @@ export class AppPreviewPanelComponent implements OnInit {
   constructor(private getPicService: GetPicturesService) { }
 
   private getImages() {
-    this.getPicService.getPictureList()
+    this.pictureList = this.getPicService.getPictureList();
+      /*
       .subscribe(
         data => { this.pictureList = data; },
         error => { this.error = error; }
         );
+        */
   }
 
   ngOnInit() {
